993c9074363418617a1ba3faed7764c3374fb173
Konrad Kleine <kkleine@redhat.com>
2023-09-19 08:58:26 +0000

    Insert at `endbb->end()` as before.
    Addresses: https://reviews.llvm.org/D159418#4637384

--- llvm/examples/BrainF/BrainF.cpp
+++ llvm/examples/BrainF/BrainF.cpp
@@ -126,7 +126,7 @@ void BrainF::header(LLVMContext& C) {
   endbb = BasicBlock::Create(C, label, brainf_func);
 
   //call free(i8 *%arr)
-  builder->SetInsertPoint(endbb->getTerminator());
+  builder->SetInsertPoint(endbb->end());
   builder->CreateFree(ptr_arr);
   
 
